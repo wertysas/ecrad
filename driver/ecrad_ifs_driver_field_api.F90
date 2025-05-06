a9d7e101414f447094fbb356589db7b66d75d38a
Johan Ericsson <johan.ericsson@ecmwf.int>
2025-05-06 16:17:22 +0200

    add lw/sw path and loki remove pragmas to ifs field api driver

--- driver/ecrad_ifs_driver_field_api.F90
+++ driver/ecrad_ifs_driver_field_api.F90
@@ -326,21 +326,26 @@ program ecrad_ifs_driver
   call flux%allocate(yradiation%rad_config, 1, ncol, nlev)
 
   ! set relevant fluxes to zero
-  flux%lw_up(:,:) = 0._jprb
-  flux%lw_dn(:,:) = 0._jprb
-  flux%sw_up(:,:) = 0._jprb
-  flux%sw_dn(:,:) = 0._jprb
-  flux%sw_dn_direct(:,:) = 0._jprb
-  flux%lw_up_clear(:,:) = 0._jprb
-  flux%lw_dn_clear(:,:) = 0._jprb
-  flux%sw_up_clear(:,:) = 0._jprb
-  flux%sw_dn_clear(:,:) = 0._jprb
-  flux%sw_dn_direct_clear(:,:) = 0._jprb
-
-  flux%lw_dn_surf_canopy(:,:) = 0._jprb
-  flux%sw_dn_diffuse_surf_canopy(:,:) = 0._jprb
-  flux%sw_dn_direct_surf_canopy(:,:) = 0._jprb
-  flux%lw_derivatives(:,:) = 0._jprb
+  !$loki remove
+  if (yradiation%rad_config%do_sw) then
+    flux%sw_up(:,:) = 0._jprb
+    flux%sw_dn(:,:) = 0._jprb
+    flux%sw_dn_direct(:,:) = 0._jprb
+    flux%sw_up_clear(:,:) = 0._jprb
+    flux%sw_dn_clear(:,:) = 0._jprb
+    flux%sw_dn_direct_clear(:,:) = 0._jprb
+    flux%sw_dn_diffuse_surf_canopy(:,:) = 0._jprb
+    flux%sw_dn_direct_surf_canopy(:,:) = 0._jprb
+  end if
+  !$loki end remove
+  if (yradiation%rad_config%do_lw) then
+    flux%lw_up(:,:) = 0._jprb
+    flux%lw_dn(:,:) = 0._jprb
+    flux%lw_up_clear(:,:) = 0._jprb
+    flux%lw_dn_clear(:,:) = 0._jprb
+    flux%lw_dn_surf_canopy(:,:) = 0._jprb
+    flux%lw_derivatives(:,:) = 0._jprb
+  end if
 
   ! Allocate memory for additional arrays
   allocate(ccn_land(ncol))
@@ -422,21 +427,24 @@ program ecrad_ifs_driver
   ! "up" fluxes are actually net fluxes at this point - we modify the
   ! upwelling flux so that net=dn-up, while the TOA and surface
   ! downwelling fluxes are correct.
-  flux%sw_up = -flux%sw_up
-  flux%sw_up(:,1) = flux%sw_up(:,1)+flux%sw_dn(:,1)
-  flux%sw_up(:,nlev+1) = flux%sw_up(:,nlev+1)+flux%sw_dn(:,nlev+1)
-
-  flux%lw_up = -flux%lw_up
-  flux%lw_up(:,1) = flux%lw_up(:,1)+flux%lw_dn(:,1)
-  flux%lw_up(:,nlev+1) = flux%lw_up(:,nlev+1)+flux%lw_dn(:,nlev+1)
-
-  flux%sw_up_clear = -flux%sw_up_clear
-  flux%sw_up_clear(:,1) = flux%sw_up_clear(:,1)+flux%sw_dn_clear(:,1)
-  flux%sw_up_clear(:,nlev+1) = flux%sw_up_clear(:,nlev+1)+flux%sw_dn_clear(:,nlev+1)
-
-  flux%lw_up_clear = -flux%lw_up_clear
-  flux%lw_up_clear(:,1) = flux%lw_up_clear(:,1)+flux%lw_dn_clear(:,1)
-  flux%lw_up_clear(:,nlev+1) = flux%lw_up_clear(:,nlev+1)+flux%lw_dn_clear(:,nlev+1)
+  !$loki remove
+  if (yradiation%rad_config%do_sw) then
+    flux%sw_up = -flux%sw_up
+    flux%sw_up(:,1) = flux%sw_up(:,1)+flux%sw_dn(:,1)
+    flux%sw_up(:,nlev+1) = flux%sw_up(:,nlev+1)+flux%sw_dn(:,nlev+1)
+    flux%sw_up_clear = -flux%sw_up_clear
+    flux%sw_up_clear(:,1) = flux%sw_up_clear(:,1)+flux%sw_dn_clear(:,1)
+    flux%sw_up_clear(:,nlev+1) = flux%sw_up_clear(:,nlev+1)+flux%sw_dn_clear(:,nlev+1)
+  end if
+  !$loki end remove
+  if (yradiation%rad_config%do_lw) then
+    flux%lw_up = -flux%lw_up
+    flux%lw_up(:,1) = flux%lw_up(:,1)+flux%lw_dn(:,1)
+    flux%lw_up(:,nlev+1) = flux%lw_up(:,nlev+1)+flux%lw_dn(:,nlev+1)
+    flux%lw_up_clear = -flux%lw_up_clear
+    flux%lw_up_clear(:,1) = flux%lw_up_clear(:,1)+flux%lw_dn_clear(:,1)
+    flux%lw_up_clear(:,nlev+1) = flux%lw_up_clear(:,nlev+1)+flux%lw_dn_clear(:,nlev+1)
+  end if
 
   ! --------------------------------------------------------
   ! Section 5: Check and save output

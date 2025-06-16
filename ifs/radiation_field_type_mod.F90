204f93066e81e968d694628459fa9e6a4123b537
Johan Ericsson <johan.ericsson@ecmwf.int>
2025-06-16 16:26:13 +0200

    add field api wrappers of ecrad derived types: thermodynamics + field type clean

--- ifs/radiation_field_type_mod.F90
+++ ifs/radiation_field_type_mod.F90
@@ -24,7 +24,8 @@ module radiation_field_type_module
   use field_factory_module
 
   ! radiation imports
-  use radiation_single_level,   only: single_level_type
+  use radiation_single_level,     only: single_level_type
+  use radiation_thermodynamics,   only: thermodynamics_type
 
   implicit none
 
@@ -60,10 +61,11 @@ module radiation_field_type_module
     class(field_3rb), pointer :: &
          &   f_lw_emission=>null()         ! (ncol,nemissbands,nblocks)
     class(field_2rb), pointer :: &
-         &   f_spectral_solar_scaling=>null() ! (n_bands_sw, nblocks)
-    class(field_2im), pointer :: f_iseed=>null() ! (ncol, nblocks)
+         &   f_spectral_solar_scaling=>null() ! (n_bands_sw,nblocks)
+    class(field_2im), pointer :: f_iseed=>null() ! (ncol,nblocks)
 
     logical                   :: is_simple_surface = .true.
+
   contains
 
     procedure :: init => single_level_field_init
@@ -73,9 +75,35 @@ module radiation_field_type_module
 
   end type single_level_field_type
 
+  type thermodynamics_field_type
+
+     real(jprb), pointer, dimension(:,:) :: &
+          &  pressure_hl=>null(), &   ! (ncol,nlev+1) pressure (Pa)
+          &  temperature_hl=>null()   ! (ncol,nlev+1) temperature (K)
+     real(jprb), pointer, dimension(:,:) :: &
+          &  h2o_sat_liq=>null() ! (ncol,nlev) specific humidity at liquid
+                         ! saturation (kg/kg)
+     class(field_3rb), pointer :: &
+          &  f_pressure_hl=>null(), &   ! (ncol,nlev+1,nblocks) pressure (Pa)
+          &  f_temperature_hl=>null()   ! (ncol,nlev+1,nblocks) temperature (K)
+     class(field_3rb), pointer :: &
+          &  f_h2o_sat_liq=>null() ! (ncol,nlev,nblocks) specific humidity at liquid
+                         ! saturation (kg/kg)
+
+   contains
+
+    procedure :: init => thermodynamics_field_init
+    procedure :: final => thermodynamics_field_final
+    procedure :: update_view => thermodynamics_field_update_view
+    procedure :: update_thermodynamics => thermodynamics_field_update_thermodynamics
+
+  end type thermodynamics_field_type
 
 contains
 
+
+!-----------------------------------------------------------------------
+! single_level_field_type procedures
 
   !---------------------------------------------------------------------
   ! Initialise single_field_type
@@ -133,41 +161,49 @@ contains
     if (associated(this%f_cos_sza)) then
       call field_delete(this%f_cos_sza)
     end if
+    this%f_cos_sza => null()
     this%cos_sza => null()
 
     if (associated(this%f_skin_temperature)) then
       call field_delete(this%f_skin_temperature)
     end if
+    this%f_skin_temperature => null()
     this%skin_temperature => null()
 
     if (associated(this%f_sw_albedo)) then
       call field_delete(this%f_sw_albedo)
     end if
+    this%f_sw_albedo => null()
     this%sw_albedo => null()
 
     if (associated(this%f_sw_albedo_direct)) then
       call field_delete(this%f_sw_albedo_direct)
     end if
+    this%f_sw_albedo_direct => null()
     this%sw_albedo_direct => null()
 
     if (associated(this%f_lw_emissivity)) then
       call field_delete(this%f_lw_emissivity)
     end if
+    this%f_lw_emissivity => null()
     this%lw_emissivity => null()
 
     if (associated(this%f_lw_emission)) then
       call field_delete(this%f_lw_emission)
     end if
+    this%f_lw_emission => null()
     this%lw_emission => null()
 
     if (associated(this%f_spectral_solar_scaling)) then
       call field_delete(this%f_spectral_solar_scaling)
     end if
+    this%f_spectral_solar_scaling => null()
     this%spectral_solar_scaling => null()
 
     if (associated(this%f_iseed)) then
       call field_delete(this%f_iseed)
     end if
+    this%f_iseed => null()
     this%iseed => null()
 
     if (lhook) call dr_hook('radiation_field_type:single_level_field_final',1,hook_handle)
@@ -217,7 +253,7 @@ contains
 
     real(jphook) :: hook_handle
 
-    if (lhook) call dr_hook('radiation_field_type:field_update_single_level',0,hook_handle)
+    if (lhook) call dr_hook('radiation_field_type:single_level_field_update_single_level',0,hook_handle)
 
     if (associated(this%cos_sza)) single_level%cos_sza => this%cos_sza
 
@@ -235,8 +271,131 @@ contains
 
     single_level%is_simple_surface = this%is_simple_surface
 
-    if (lhook) call dr_hook('radiation_field_type:field_update_single_level',1,hook_handle)
+    if (lhook) call dr_hook('radiation_field_type:single_level_field_update_single_level',1,hook_handle)
 
   end subroutine single_level_field_update_single_level
 
+
+!-----------------------------------------------------------------------
+! thermodynamics_field_type procedures
+
+  !---------------------------------------------------------------------
+  ! Allocate variables with specified dimensions
+  subroutine thermodynamics_field_init(this, nblocks, ncol, nlev, use_h2o_sat)
+
+    use yomhook,  only : lhook, dr_hook, jphook
+
+    class(thermodynamics_field_type), intent(inout) :: this
+    integer, intent(in)           :: nblocks  ! Number of blocks
+    integer, intent(in)           :: ncol  ! Number of columns
+    integer, intent(in)           :: nlev  ! Number of levels
+    logical, intent(in), optional :: use_h2o_sat ! Allocate h2o_sat_liq?
+
+    logical :: use_h2o_sat_local
+
+    real(jphook) :: hook_handle
+
+    if (lhook) call dr_hook('radiation_field_type:thermodynamics_field_init',0,hook_handle)
+
+    call field_new(this%f_pressure_hl, ubounds=[ncol,nlev+1, nblocks], persistent=.true.)
+    call field_new(this%f_temperature_hl, ubounds=[ncol,nlev+1, nblocks], persistent=.true.)
+
+    use_h2o_sat_local = .false.
+    if (present(use_h2o_sat)) then
+      use_h2o_sat_local = use_h2o_sat
+    end if
+
+    if (use_h2o_sat_local) then
+      call field_new(this%f_h2o_sat_liq, ubounds=[ncol,nlev, nblocks], persistent=.true.)
+    end if
+
+    if (lhook) call dr_hook('radiation_field_type:thermodynamics_field_init',1,hook_handle)
+
+  end subroutine thermodynamics_field_init
+
+  !---------------------------------------------------------------------
+  ! Deallocate variables
+  subroutine thermodynamics_field_final(this)
+
+    use yomhook,  only : lhook, dr_hook, jphook
+
+    class(thermodynamics_field_type), intent(inout) :: this
+
+    real(jphook) :: hook_handle
+
+    if (lhook) call dr_hook('radiation_thermodynamics:thermodynamics_field_final',0,hook_handle)
+
+    if (associated(this%f_pressure_hl)) then
+      call field_delete(this%f_pressure_hl)
+    end if
+    this%f_pressure_hl=>null()
+    this%pressure_hl=>null()
+    if (associated(this%f_temperature_hl)) then
+      call field_delete(this%f_temperature_hl)
+    end if
+    this%f_temperature_hl=>null()
+    this%temperature_hl=>null()
+    if (associated(this%f_h2o_sat_liq)) then
+      call field_delete(this%f_h2o_sat_liq)
+    end if
+    this%f_h2o_sat_liq=>null()
+    this%h2o_sat_liq=>null()
+
+    if (lhook) call dr_hook('radiation_thermodynamics:thermodynamics_field_final',1,hook_handle)
+
+  end subroutine thermodynamics_field_final
+
+  !---------------------------------------------------------------------
+  ! Update view pointers with block level views
+  subroutine thermodynamics_field_update_view(this, block_index)
+
+    use yomhook,  only : lhook, dr_hook, jphook
+
+    class(thermodynamics_field_type), intent(inout) :: this
+    integer,                          intent(in)    :: block_index
+
+    real(jphook) :: hook_handle
+
+    if (lhook) call dr_hook('radiation_thermodynamics:thermodynamics_field_update_view',0,hook_handle)
+
+    if (associated(this%f_pressure_hl)) then
+      this%pressure_hl => this%f_pressure_hl%get_view(block_index)
+    end if
+    if (associated(this%f_temperature_hl)) then
+      this%temperature_hl => this%f_temperature_hl%get_view(block_index)
+    end if
+    if (associated(this%f_h2o_sat_liq)) then
+      this%h2o_sat_liq => this%f_h2o_sat_liq%get_view(block_index)
+    end if
+
+    if (lhook) call dr_hook('radiation_thermodynamics:thermodynamics_field_update_view',1,hook_handle)
+
+  end subroutine thermodynamics_field_update_view
+
+  !---------------------------------------------------------------------
+  ! Update thermodynamics type with the view pointers of this object
+  subroutine thermodynamics_field_update_thermodynamics(this, thermodynamics)
+
+    use yomhook,  only : lhook, dr_hook, jphook
+
+    class(thermodynamics_field_type), intent(inout) :: this
+    class(thermodynamics_type),       intent(inout) :: thermodynamics
+
+    real(jphook) :: hook_handle
+
+    if (lhook) call dr_hook('radiation_thermodynamics:thermodynamics_field_update_thermodynamics',0,hook_handle)
+
+    if (associated(this%f_pressure_hl)) then
+      thermodynamics%pressure_hl => this%pressure_hl
+    end if
+    if (associated(this%f_temperature_hl)) then
+      thermodynamics%temperature_hl => this%temperature_hl
+    end if
+    if (associated(this%f_h2o_sat_liq)) then
+      thermodynamics%h2o_sat_liq => this%h2o_sat_liq
+    end if
+
+    if (lhook) call dr_hook('radiation_thermodynamics:thermodynamics_field_update_thermodynamics',1,hook_handle)
+
+  end subroutine thermodynamics_field_update_thermodynamics
 end module radiation_field_type_module

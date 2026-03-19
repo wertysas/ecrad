ddd3a1cb5959a4d44d059870aa096f8560bc6dfe
Johan Ericsson <johan.ericsson@ecmwf.int>
2026-03-19 16:59:14 +0000

    FIXME: Hard coded nproma = 32 for Loki variants

--- driver/ecrad_ifs_driver_field_api.F90
+++ driver/ecrad_ifs_driver_field_api.F90
@@ -171,7 +171,11 @@ program ecrad_ifs_driver
 
   ! Read "radiation_driver" namelist into radiation driver config type
   call driver_config%read(file_name)
+  #if defined(HAVE_LOKI)
+  nproma = 32
+  #else
   nproma = driver_config%nblocksize
+  #endif
 
   if (driver_config%iverbose >= 2) then
     write(nulout,'(a)') '-------------------------- OFFLINE ECRAD RADIATION SCHEME --------------------------'
